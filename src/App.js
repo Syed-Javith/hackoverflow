43e1d4d4501687e8eda8bab103b52c6928124aca
Sangeetha2125 <gsangeetha7113@gmail.com>
2023-09-21 21:32:52 +0530

    Domain Card Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route, BrowserRouter as Router} from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Domain from "./components/Domain/Domain";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Domain />
     </Router>
   );
 }
---
+++ src/components/Domain/Domain.jsx
@@ -0,0 +1,18 @@
+import './Domain.css'
+import DomainCard from './DomainCard'
+
+function Domain(){
+
+    return (
+        <div className="domain-container">
+            <DomainCard />
+            <DomainCard />
+            <DomainCard />
+            <DomainCard />
+            <DomainCard />
+            <DomainCard />
+        </div>
+    )
+}
+
+export default Domain
