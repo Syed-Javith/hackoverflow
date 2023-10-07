30fa292652594977a17c002d0e96e4aa9227909d
Syed-Javith <syedjavith14@gmail.com>
2023-10-07 21:31:22 +0530

    f4cexc

--- src/components/Navigationbar/NavigationBar.jsx
+++ src/components/Navigationbar/NavigationBar.jsx
@@ -1,20 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import './NavigationBar.css'
 function Navigationbar(){
     return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"></Navbar.Brand>
+
+      <Navbar collapseOnSelect expand="lg">
+      <Container>
+      <Navbar.Brand href="#home"><img id='nav-img' src={require('../../images/logo_sym.png')}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">About Us</Nav.Link>
+            <Nav.Link href="#pricing">Domains</Nav.Link>
+            <Nav.Link href="#pricing">Contact Us</Nav.Link>
           </Nav>
-        </Container>
-      </Navbar>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
     );
 }
